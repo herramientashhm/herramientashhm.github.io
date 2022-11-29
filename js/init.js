6ddb9fec97d2db076c26742202a3607396290bae
herramientashhm <119427875+herramientashhm@users.noreply.github.com>
2022-11-29 08:28:42 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,18 +1,21 @@
-/*  Inicializa Firebase con la
- * configuración del proyecto.
- * Revisa la configuración en tu
- * servidor de Firebase y cópiala
- * aquí sustituyendo los
- * asteriscos. Los campos deben
- * quedar igual que en tu
- * servidor. */
-// @ts-ignore
-firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
-});
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCzVwbWNHBpiAg9MOZBy4UHQxIH-cU_RS0",
+  authDomain: "herramientashhm-fe8b6.firebaseapp.com",
+  projectId: "herramientashhm-fe8b6",
+  storageBucket: "herramientashhm-fe8b6.appspot.com",
+  messagingSenderId: "920913104335",
+  appId: "1:920913104335:web:9188ac0d5d7c39e0a516ba",
+  measurementId: "G-NSLRR20EEP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
